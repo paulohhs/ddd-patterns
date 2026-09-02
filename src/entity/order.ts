bed07a27da6bbdf6b8f796c158e07f199250b5f0
paulohhs <paulohhonoratos@gmail.com>
2026-09-02 17:29:40 -0300

    entidades: adiciona entidade order.

---
+++ src/entity/order.ts
@@ -0,0 +1,35 @@
+import OrderItem from "./order_item";
+
+export default class Order {
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[] = [];
+    private _total: number;
+
+    constructor(id: string, customerId: string, items: OrderItem[]) {
+        this._id = id;
+        this._customerId = customerId;
+        this._items = items;
+        this._total = this.total();
+        this.validate();
+    }
+
+    validate(): boolean {
+        if (this._id.length === 0) {
+            throw new Error("ID is required");
+        }
+        if (this._customerId.length === 0) {
+            throw new Error("CustomerId is required");
+        }
+
+        if (this._items.length === 0) {
+            throw new Error("Item quantity must be greater than 0");
+        }
+
+        return true
+    }
+
+    total(): number {
+        return this._items.reduce((acc, item) => acc + item._price, 0);
+    }
+}
