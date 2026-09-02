bbeaa1054724631cfed905ae7752bb4356c5f2bb
paulohhs <paulohhonoratos@gmail.com>
2026-09-02 19:11:39 -0300

    refactor(src/entity/order.ts): adiciona validação para quantidade de items.

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -26,10 +26,14 @@ export default class Order {
             throw new Error("Item quantity must be greater than 0");
         }
 
-        return true
+        if (this._items.some((item) => item.quantity <= 0)) {
+            throw new Error("Quantity must be greater than 0");
+        }
+
+        return true;
     }
 
     total(): number {
-        return this._items.reduce((acc, item) => acc + item._price, 0);
+        return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
     }
 }
