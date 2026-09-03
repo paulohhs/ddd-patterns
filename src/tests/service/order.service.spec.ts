bec743b446807989b43ba76bf5480705dd3ef035
paulohhs <paulohhonoratos@gmail.com>
2026-09-02 21:39:27 -0300

    testes(src/tests/service/order.service.spec.ts): adiciona teste unitário para programa de rewards.

--- src/tests/service/order.service.spec.ts
+++ src/tests/service/order.service.spec.ts
@@ -4,6 +4,16 @@ import OrderItem from "../../entity/order_item";
 import OrderService from "../../service/order.service";
 
 describe("Order service unit tests", () => {
+    it("should place an order", () => {
+        const customer = new Customer("1", "Customer 1");
+        const item1 = new OrderItem("1", "Order Item 1", 10, "p1", 1);
+
+        const order = OrderService.placeOrder(customer, [item1]);
+
+        expect(customer.rewardPoints).toBe(5);
+        expect(order.total()).toBe(10);
+    });
+    
     it("should get total of all orders", () => {
         const item1 = new OrderItem("1", "Order Item 1", 100, "p1", 1);
         const item2 = new OrderItem("2", "Order Item 2", 200, "p2", 2);
